09ea199bea9c29ff3e4f503df3bc202e7fa9ddec
Greerso <greerso@voskcoin.io>
2018-04-15 22:34:29 -0400

    Added ccminerForks.sh one line miner replacement script

--- ccminerForks.sh
+++ ccminerForks.sh
@@ -8,97 +8,51 @@ fi
 #
 # Helper Functions
 #
+
+# Ask user for input, offer a default value example use:
+# echo "Please enter a value: (Default: new_value)"
+# newValue=$(inputWithDefault new_value)
 inputWithDefault() {
     read -r userInput
     userInput=${userInput:-$@}
     echo "$userInput"
 }
 
-
 #
 # Set Variables
 #
 
 # Location of smOS miners
+minerRoot='/root/miner_org/'
+smosMiners="$(find $minerRoot -name 'ccminer' -printf '%h\n' | sed 's!.*/!!' | sort -u)"
+# minerRepo='https://github.com/greerso/scrypts/raw/master/miners/'
+githubMiners="$(curl -s https://api.github.com/repos/greerso/scrypts/contents/miners | jq '.[].name')"
+githubURLs="$(curl -s https://api.github.com/repos/greerso/scrypts/contents/miners | jq '.[].download_url')"
 
-minerPath='/root/miner_org/'
-minerRepo=''
+# Which smOS miner to replace?
+printf "Please select the miner to replace:\n"
+select smosMiner in $smosMiners; do test -n "$smosMiner" && break; echo ">>> Invalid Selection"; done
 
-# Which miner to replace?
-PS3='Which smOS ccminer would you like to replace?: '
-options=("enemy" "MSFTserver-v2.2.5" "nevermore" "skunk-krnlx" "todd1251-x16r" "tpruvot-v2.1" "tpruvot-v2.2.5-x16s" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "nevermore-brian112358-v0.1")
-            smosMiner="nevermore-brian112358-v0.1"
-            ;;
-        "MSFTserver-v2.2.5")
-            smosMiner="MSFTserver-v2.2.5"
-            ;;
-        "skunk-krnlx")
-            smosMiner="skunk-krnlx"
-            ;;
-        "todd1251-x16r-2.2.4.1")
-            smosMiner="todd1251-x16r-2.2.4.1"
-            ;;
-        "tpruvot-v2.1")
-            smosMiner="tpruvot-v2.1"
-            ;;
-        "tpruvot-v2.2.5-x16s-cuda8.0")
-            smosMiner="tpruvot-v2.2.5-x16s-cuda8.0"
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo invalid option;;
-    esac
-done
+echo "You selected $smosMiner"
 
 clear
 
-# What fork of ccminer?
-PS3='Please enter the ccminer fork that you would like to replace $smosMiner with: '
-options=("allium" "bcd" "dace-cryptonight" "djm34" "klaust" "nanashi" "nevermore" "phi-anxmod" "rvn" "sp-mod" "tpruvot" "xevan" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "allium-2.3.0")
-            minerFork="allium-2.3.0"
-            ;;
-        "bcd-1.0.0 ")
-            minerFork="bcd-1.0.0 "
-            ;;
-        "dace-cryptonight-20180113")
-            minerFork="dace-cryptonight-20180113"
-            ;;
-         "djm34-0.3.0 ")
-            minerFork="djm34-0.3.0 "
-            ;;
-        "klaust-8.21")
-            minerFork="klaust-8.21"
-            ;;
-        "nanashi-2.2.mod.r2")
-            minerFork="nanashi-2.2.mod.r2"
-            ;;
-        "nevermore-0.2.2")
-            minerFork="nevermore-0.2.2"
-            ;;
-        "phi-anxmod-1.0-cuda9")
-            minerFork="phi-anxmod-1.0-cuda9"
-            ;;
-        "rvn-2.3")
-            minerFork="rvn-2.3"
-            ;;
-        "sp-mod-1.5.81")
-            minerFork="sp-mod-1.5.81"
-            ;;
-        "xevan-0.1")
-            minerFork="xevan-0.1"
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo invalid option;;
-    esac
-done
+# Which miner to replace $smosMiner with?
+printf "Please select the miner to replace $smosMiner with:\n"
+select minerFork in $githubMiners; do test -n "$minerFork" && break; echo ">>> Invalid Selection"; done
+clear
+echo "You're replacing $smosMiner with $minerFork"
+
+#
+# Download miner
+#
+
+minerForkURL="$(curl -s https://api.github.com/repos/greerso/scrypts/contents/miners | jq '.[] | select(.name=="$minerFork") | .download_url')"
+
+mv $smosMiner/ccminer $smosMiner/ccminer.backup
+curl -fsSL $minerForkURL | gunzip > $smosMiner/ccminer
+chmod +x $smosMiner/ccminer
+
+clear
+
+echo "$smosMiner is now $minerFork!  You should now configure your Rig Group for $smosMiner remembering that it is $minerFork"
